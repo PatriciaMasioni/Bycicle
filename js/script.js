1a6bd8f8889a4fb2486e19a045aa70f0294cf3e2
Patrcia Masioni <33434507+PatriciaMasioni@users.noreply.github.com>
2018-04-25 17:11:28 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,16 +4,18 @@
 $(function() {
 
 /*
+
   $("h2#home").mouseover(function() {
     $("h2.home,h3.home").fadeOut("slow");
   });
-*/
 
 $("p.about").css("display","none");
 
 $("h3").on("mouseover", function() {
   $("p.about").slideDown("slow");
 });
+
+*/
 
 $("img.store").elevateZoom({
   zoomType: "inner",
