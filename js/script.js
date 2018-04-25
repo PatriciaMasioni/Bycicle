c83446e7c1a1a4883076e3954e6c1b5c369f484a
Brioche <33434507+PatriciaMasioni@users.noreply.github.com>
2018-04-25 14:36:11 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+$(function() {
+
+/*
+  $("h2#home").mouseover(function() {
+    $("h2.home,h3.home").fadeOut("slow");
+  });
+*/
+
+$("p.about").css("display","none");
+
+$("h3").on("mouseover", function() {
+  $("p.about").slideDown("slow");
+});
+
+$("img.store").elevateZoom({
+  zoomType: "inner",
+  cursor: "crosshair",
+});
+
+});
